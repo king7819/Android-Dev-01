4b7145f2dc8426363c76321367884a882845def9
king7819 <king7819@mmonstar.co.kr>
2018-12-27 13:41:03 +0900

    Hello Android

--- app/src/main/java/dev/woong/devhello/MainActivity.java
+++ app/src/main/java/dev/woong/devhello/MainActivity.java
@@ -1,7 +1,11 @@
 package dev.woong.devhello;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 버튼 클릭 이벤트
+     * @param v
+     */
+    public void onButton1Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v) {
+        Intent myIntent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivity(myIntent);
     }
 }
